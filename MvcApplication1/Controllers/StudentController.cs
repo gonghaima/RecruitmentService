a577bcae48b05848c163a4fbe6ebeac69e1766e4
gonghaima <gonghaima@hotmail.com>
2014-07-03 22:22:12 +1200

    original moved from folder18

---
+++ MvcApplication1/Controllers/StudentController.cs
@@ -0,0 +1,52 @@
+﻿using MvcApplication1.Models.ViewModels;
+using System.Web.Mvc;
+namespace MvcApplication1.Controllers
+{
+    public class StudentController : Controller
+    {
+        //
+        // GET: /Student/
+
+        [MvcApplication1.MvcApplication.OptionalAuthorize(Roles = "Student")]
+        //[MvcApplication1.MvcApplication.OptionalAuthorize(Roles = "Employers")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(Student student)
+        {
+            if (ModelState.IsValid&&student.register())
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(student);
+            }
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Student student)
+        {
+            if (student.login())
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
---
+++ MvcApplication1/Models/ViewModels/RegisterInterface.cs
@@ -0,0 +1,15 @@
+﻿
+using MvcApplication1.Models.DBModels;
+namespace MvcApplication1.Models.ViewModels
+{
+    public interface IRegisterInterface
+    {
+        bool register();
+    }
+
+    public interface IUserInterface
+    {
+        bool login();
+        void logout();
+    }
+}
